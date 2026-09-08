3961036e94a053b4f12ef53746f3ea8d583a0a4b
Didelphidae96 <131192074+Didelphidae96@users.noreply.github.com>
2026-09-08 22:45:47 +0200

    Add files via upload

--- src/main/java/com/accbdd/agricreatecompat/AgriCreateCompat.java
+++ src/main/java/com/accbdd/agricreatecompat/AgriCreateCompat.java
@@ -1,22 +1,11 @@
 package com.accbdd.agricreatecompat;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Mixins;
 
-@Mod(AgriCreateCompat.MODID)
-public class AgriCreateCompat
-{
-    public static final String MODID = "agricreatecompat";
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public AgriCreateCompat(FMLJavaModLoadingContext context)
-    {
-        IEventBus modEventBus = context.getModEventBus();
-        MinecraftForge.EVENT_BUS.register(this);
+@Mod("agricreatecompat")
+public class AgriCreateCompat {
+    public AgriCreateCompat() {
+        // If you need to register events, do it here.
+        // Mixins load automatically via the JSON file.
     }
 }
