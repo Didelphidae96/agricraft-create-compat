ce9a90c6e6f250dff614f4186a825a3cbea0720b
Didelphidae96 <131192074+Didelphidae96@users.noreply.github.com>
2026-09-09 05:58:04 +0200

    Update AgriCreateCompat.java

--- src/main/java/com/accbdd/agricreatecompat/AgriCreateCompat.java
+++ src/main/java/com/accbdd/agricreatecompat/AgriCreateCompat.java
@@ -5,7 +5,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod("agricreatecompat")
 public class AgriCreateCompat {
     public AgriCreateCompat() {
-        // If you need to register events, do it here.
-        // Mixins load automatically via the JSON file.
+        // NeoForge compatible
     }
 }
